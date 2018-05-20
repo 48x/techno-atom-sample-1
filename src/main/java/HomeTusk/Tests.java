ffe00d4f8cb40dcb41b154e6c1374d39af728b58
DmitryNiwa <dmitry.niwa@gmail.com>
2018-05-20 14:35:38 +0300

    Dmitry Kachalin First HT updated

--- src/main/java/HomeTusk/Tests.java
+++ src/main/java/HomeTusk/Tests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
